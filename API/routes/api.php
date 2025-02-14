398f2637e532a9232ab88c23d13bc672513903b9
BoyK07 <boy.krijnen@ziggo.nl>
2025-02-14 11:01:37 +0100

    API Made in Laravel 11

---
+++ API/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\Investigation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/cases', function() {
+    return Investigation::all();
+});
